87a56f504f38a5e8ff099321a9f8d75be865f638
Christopher Brauns <chris@blocktrainapp.com>
2024-01-05 17:42:29 -0500

    Add cypress tests

--- cypress/tasks/resetDBRails.ts
+++ cypress/tasks/resetDBRails.ts
@@ -1,12 +1,6 @@
 import { post } from '@/frontend/http-common'
 
 export default async function resetDBRails() {
-  // make a post call to 3001/reset_test_database
-
-  // const response = await fetch('http://localhost:3001/create_test_user', {
-  //   method: 'POST',
-  // })
-
   const response = await post('/create_test_user', {}, '')
 
   return response.data
