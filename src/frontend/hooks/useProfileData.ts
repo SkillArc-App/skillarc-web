db961f7fb4e41ab6d4ed4c1442d3343b878a5800
Christopher Brauns <chris@blocktrainapp.com>
2024-01-22 14:41:06 -0500

    Add token to profile request

--- src/frontend/hooks/useProfileData.ts
+++ src/frontend/hooks/useProfileData.ts
@@ -1,22 +1,26 @@
-import axios from 'axios'
 import { useQuery } from 'react-query'
+import { get } from '../http-common'
 import { GetOneProfileResponse } from '../services/profile.service'
 import { mixpanelInitProfile } from '../utils/mixpanel'
+import { useAuthToken } from './useAuthToken'
 
 export const useProfileData = (id: string) => {
-  const profileQuery = useQuery(['profile', id], () => {
+  const token = useAuthToken()
+
+  const profileQuery = useQuery(['profile', id, token], () => {
     if (!id) return
 
-    return getOne(id)
+    return getOne(id, token)
   })
 
   return { profileQuery }
 }
 
-const getOne = async (id: string) => {
-  const res = await axios
-    .create({ withCredentials: false })
-    .get<GetOneProfileResponse>(`${process.env.NEXT_PUBLIC_API_URL}/profiles/${id}`, {})
+const getOne = async (id: string, token: string | undefined) => {
+  const res = await get<GetOneProfileResponse>(
+    `${process.env.NEXT_PUBLIC_API_URL}/profiles/${id}`,
+    token,
+  )
 
   mixpanelInitProfile(res.data)
 
