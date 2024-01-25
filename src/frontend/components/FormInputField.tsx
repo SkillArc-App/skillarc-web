b0392e3d0b91e48c48554b0bade4258f8293cbbb
John Chabot <johnchabot2013@gmail.com>
2024-01-25 09:03:21 -0500

    Create new chakra branded form input

---
+++ src/frontend/components/FormInputField.tsx
@@ -0,0 +1,32 @@
+import { FormControl, FormErrorMessage, FormLabel, Input } from "@chakra-ui/react"
+import { FieldHookConfig, useField } from "formik"
+import { HTMLInputTypeAttribute } from "react"
+
+type InputProps = {
+  label: string
+  placeholder?: string
+  type: HTMLInputTypeAttribute
+  isRequired?: boolean
+}
+
+type InputField<T extends InputTypes> = InputProps & FieldHookConfig<T>
+
+type InputTypes = number | string | string | undefined
+
+export default function FormInputField<T extends InputTypes>({
+  label,
+  placeholder,
+  type,
+  isRequired,
+  ...props
+}: InputField<T>) {
+  const [field, meta] = useField(props)
+
+  return (
+    <FormControl isRequired={isRequired} isInvalid={meta.touched && !!meta.error}>
+      <FormLabel>{label}</FormLabel>
+      <Input {...field} placeholder={placeholder} type={type} />
+      <FormErrorMessage>{meta.touched && meta.error}</FormErrorMessage>
+    </FormControl>
+  )
+}
