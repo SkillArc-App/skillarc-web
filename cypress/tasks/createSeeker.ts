21a53a0e264aab196a6137b18f21ddd244dcbed8
John Chabot <johnchabot2013@gmail.com>
2024-02-02 16:35:03 -0500

    Split out a seeker spec for applying / searching

--- cypress/tasks/createSeeker.ts
+++ cypress/tasks/createSeeker.ts
@@ -1,7 +1,7 @@
 import { post } from '@/frontend/http-common'
 
 export default async function createSeeker() {
-  const response = await post('/test/create_seeker', {}, '')
+  const response = await post('/test/create_seeker', {}, '', { camel: true })
 
   return response.data
 }
