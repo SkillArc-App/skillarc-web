2df14febf8ea1e23fcc6097881a70d0f847fd759
John Chabot <johnchabot2013@gmail.com>
2024-02-08 18:38:42 -0500

    Remove custom fonts for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,16 +37,6 @@ export default function RootLayout({
       <head>
         <title>SkillArc</title>
         <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        {/* <link
-          href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://api.fontshare.com/v2/css?f[]=supreme@501,2,800,400,401,200,100,300,101,301,500,201,801,1,701,700&display=swap"
-          rel="stylesheet"
-          ></link>*/}
       </head>
       <body>
         <QueryClientProvider client={queryClient}>
