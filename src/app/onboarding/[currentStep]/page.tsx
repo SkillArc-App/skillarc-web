c4d8265f630289c32aabb43ba67c1e0a45b0321e
Christopher Brauns <chris@blocktrainapp.com>
2024-02-20 15:22:46 -0500

    Remove personal experience entirely

--- src/app/onboarding/[currentStep]/page.tsx
+++ src/app/onboarding/[currentStep]/page.tsx
@@ -10,7 +10,6 @@ import { Employment } from '@/frontend/modules/onBoarding/components/onBoardingQ
 import { NewEducation } from '@/frontend/modules/onBoarding/components/onBoardingQuestions/NewEductation.component'
 import { NewName } from '@/frontend/modules/onBoarding/components/onBoardingQuestions/NewName.component'
 import { NewOpportunityInterests } from '@/frontend/modules/onBoarding/components/onBoardingQuestions/NewOpportunityInterests.component'
-import { Other } from '@/frontend/modules/onBoarding/components/onBoardingQuestions/Other.component'
 import { Reliability } from '@/frontend/modules/onBoarding/components/onBoardingQuestions/Reliability.component'
 import { Training } from '@/frontend/modules/onBoarding/components/onBoardingQuestions/Training.component'
 import { Flex, Progress } from '@chakra-ui/react'
@@ -86,14 +85,6 @@ const Onboarding = () => {
       return
     }
 
-    if (
-      reliability.includes("I have other experience I'd like to share") &&
-      !onboardingData.responses.other
-    ) {
-      routeToSlug('other')
-      return
-    }
-
     if (!onboardingData.responses.opportunityInterests) {
       routeToSlug('opportunities')
       return
@@ -131,7 +122,7 @@ const Onboarding = () => {
         responses: newOnboardingResponse.responses,
       },
       token,
-      { camel: false }
+      { camel: false },
     ).then((_) => {
       refetchOnboardingData()
     })
@@ -146,7 +137,6 @@ const Onboarding = () => {
     if (currentStep === 'employment') return <Employment onSubmit={onSubmit} />
     if (currentStep === 'training') return <Training onSubmit={onSubmit} />
     if (currentStep === 'education') return <NewEducation onSubmit={onSubmit} />
-    if (currentStep === 'other') return <Other onSubmit={onSubmit} />
     if (currentStep === 'opportunities' || currentStep === 'complete') {
       return <NewOpportunityInterests onSubmit={onSubmit} />
     }
@@ -172,10 +162,6 @@ const Onboarding = () => {
     }
 
     if (currentStep === 'education') {
-      return 70
-    }
-
-    if (currentStep === 'other') {
       return 80
     }
 
--- src/frontend/services/profile.service.ts
+++ src/frontend/services/profile.service.ts
@@ -16,6 +16,7 @@ export type Profile = {
 }
 
 export type GetOneProfileResponse = {
+  about: string
   industryInterests: string[]
   isProfileEditor: boolean
   user: User & {
