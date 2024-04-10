91dc8041784e2f88106fa016c6eb93eb5acad692
Christopher Brauns <chris@blocktrainapp.com>
2024-04-10 12:30:07 -0400

    Add token to job fetch

--- src/frontend/hooks/useAdminJobData.ts
+++ src/frontend/hooks/useAdminJobData.ts
@@ -1,14 +1,14 @@
 import { Maybe } from '@/common/types/maybe'
-import { useQuery } from 'react-query'
 import { get } from '../http-common'
 import { Job } from '../services/jobs.service'
+import { useAuthenticatedQuery } from './useAuthenticatedQuery'
 
 export const useAdminJobData = (id: Maybe<string>) => {
-  const getOneJob = useQuery(
+  const getOneJob = useAuthenticatedQuery(
     ['job', id],
-    () => {
+    ({ token }) => {
       const getOne = async (jobId: string) => {
-        const res = await get<Job>(`${process.env.NEXT_PUBLIC_API_URL}/admin/jobs/${jobId}`)
+        const res = await get<Job>(`${process.env.NEXT_PUBLIC_API_URL}/admin/jobs/${jobId}`, token)
 
         return res.data
       }
