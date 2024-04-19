1e1c5ce343374850737aac97d9c110ef0b56eee4
John Chabot <johnchabot2013@gmail.com>
2024-04-19 16:12:20 -0400

    Add specific seeker tasks query

--- src/app/coaches/hooks/useCoachTasks.ts
+++ src/app/coaches/hooks/useCoachTasks.ts
@@ -12,3 +12,23 @@ export const useCoachTasks = () =>
 
     return getCoachTasks()
   })
+
+export const useCoachSeekerTasks = (id?: string) =>
+  useAuthenticatedQuery(
+    ['coachTasks', id],
+    ({ token }) => {
+      const getCoachTasks = async () => {
+        const res = await get<CoachTask[]>(
+          `${process.env.NEXT_PUBLIC_API_URL}/coaches/tasks`,
+          token,
+          undefined,
+          { context_id: id }
+        )
+
+        return res.data
+      }
+
+      return getCoachTasks()
+    },
+    { enabled: !!id },
+  )
