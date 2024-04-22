35f362ef62f8f9bf340f3b9f0bce4e4ebf733323
John Chabot <johnchabot2013@gmail.com>
2024-04-22 14:41:26 -0400

    Separate Job Types and update hooks

---
+++ src/app/admin/hooks/useAdminJob.ts
@@ -0,0 +1,22 @@
+import { Maybe } from '@/common/types/maybe'
+import { useAuthenticatedQuery } from '../../../frontend/hooks/useAuthenticatedQuery'
+import { get } from '../../../frontend/http-common'
+import { AdminJob } from '../../../frontend/services/jobs.service'
+
+export const useAdminJob = (id: Maybe<string>) =>
+  useAuthenticatedQuery(
+    ['job', id],
+    ({ token }) => {
+      const getOne = async (jobId: string) => {
+        const res = await get<AdminJob>(
+          `${process.env.NEXT_PUBLIC_API_URL}/admin/jobs/${jobId}`,
+          token,
+        )
+
+        return res.data
+      }
+
+      return getOne(id as string)
+    },
+    { enabled: !!id },
+  )
