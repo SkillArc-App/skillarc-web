d94bfd951569e6b6a57d22017a5d59ab1bc7045f
John Chabot <johnchabot2013@gmail.com>
2024-04-30 15:56:33 -0400

    Add new task for no failed jobs

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,6 +10,7 @@ import createSeekerLead from 'cypress/tasks/createSeekerLead'
 import createTrainerWithStudent from 'cypress/tasks/createTrainerWithStudent'
 import createUser from 'cypress/tasks/createUser'
 import assertNoFailedJobs from 'cypress/tasks/assertNoFailedJobs'
+import clearFailedJobs from 'cypress/tasks/clearFailedJobs'
 
 export default defineConfig({
   e2e: {
@@ -21,6 +22,7 @@ export default defineConfig({
         createCoach,
         createRecruiterWithApplicant,
         createActiveSeeker,
+        clearFailedJobs,
         createSeeker,
         createJob,
         createSeekerLead,
