83899eae99443043aae59a3f601f47c63eb687c5
John Chabot <johnchabot2013@gmail.com>
2024-05-13 15:48:19 -0400

    Add ultra basic E2E test

---
+++ cypress/e2e/job_order.cy.ts
@@ -0,0 +1,44 @@
+export {}
+
+describe('Admin', () => {
+  beforeEach(() => {
+    cy.task('createJob').then((r: any) => {
+      cy.wrap(r).as('job')
+    })
+    cy.task('assertNoFailedJobs')
+  })
+
+  it('should navigate through job orders', () => {
+    cy.visit('/')
+
+    const emailSelect = cy.get('select').filter((_, element) => {
+      return !!element.innerText.match(/.*@[a-zA-z].[a-z]/)
+    })
+    emailSelect.should('be.enabled')
+    emailSelect.select('job_order@skillarc.com', { timeout: 10000 })
+
+    cy.get('@job').then((job: any) => {
+      cy.visit('/orders')
+
+      cy.findByRole('table').within(() => {
+        cy.findByText('Opened At').click()
+
+        const row = cy.findByText(job.employmentTitle).parent().parent()
+
+        row.within(() => {
+          cy.findByText('Provide Order Count')
+          cy.findByText('Needs Order Count')
+        })
+
+        cy.findByText(job.employmentTitle).click()
+      })
+
+      cy.findByText('1caea190-8810-4091-a593-203ae5990db7')
+      cy.findByText('Needs Order Count')
+      cy.findByLabelText('Order Count*').clear().type('12')
+      cy.findByRole('button', { name: 'Update' }).click()
+      cy.findByText('Order Count: 12')
+      cy.findByText('Open')
+    })
+  })
+})
