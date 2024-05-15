ecc14112380325b716418966f8a8de22d5ccd7d0
John Chabot <johnchabot2013@gmail.com>
2024-05-15 14:07:48 -0400

    Extend cypress test

--- cypress/e2e/job_order.cy.ts
+++ cypress/e2e/job_order.cy.ts
@@ -1,9 +1,10 @@
-export { }
+export {}
 
 describe('Admin', () => {
   beforeEach(() => {
     cy.task('createJob').then((r: any) => {
-      cy.wrap(r).as('job')
+      cy.wrap(r['job']).as('job')
+      cy.wrap(r['employer']).as('employer')
     })
     cy.task('assertNoFailedJobs')
   })
@@ -18,54 +19,71 @@ describe('Admin', () => {
     emailSelect.select('job_order@skillarc.com', { timeout: 10000 })
 
     cy.get('@job').then((job: any) => {
-      cy.visit('/orders')
+      cy.get('@employer').then((employer: any) => {
+        cy.visit('/orders')
 
-      // Find job order in table
-      cy.findByRole('table').within(() => {
-        cy.findByText('Opened At').click()
+        // Find job order in table
+        cy.findByRole('table').within(() => {
+          cy.findByText('Opened At').click()
 
-        const row = cy.findByText(job.employmentTitle).parent().parent().parent()
+          const row = cy.findByText(job.employmentTitle).parent().parent().parent()
 
-        row.within(() => {
-          cy.findByText('Provide Order Count')
-          cy.findByText('Needs Order Count')
+          row.within(() => {
+            cy.findByText('Provide Order Count')
+            cy.findByText('Needs Order Count')
+          })
+
+          cy.findByText(job.employmentTitle).click()
         })
 
-        cy.findByText(job.employmentTitle).click()
-      })
+        // Set order count
+        cy.findByText(job.employmentTitle)
+        cy.findByText('Needs Order Count')
+        cy.findByLabelText('Order Count*').clear().type('12')
+        cy.findByRole('button', { name: 'Update' }).click()
+        cy.findByText('Order Count: 12')
+        cy.findByText('Open')
 
-      // Set order count
-      cy.findByText(job.employmentTitle)
-      cy.findByText('Needs Order Count')
-      cy.findByLabelText('Order Count*').clear().type('12')
-      cy.findByRole('button', { name: 'Update' }).click()
-      cy.findByText('Order Count: 12')
-      cy.findByText('Open')
+        // close order
+        cy.findByText('Close Job Order Without Filling').click()
+        cy.findByText('Closed Without Filling')
 
-      // close order
-      cy.findByText('Close Job Order Without Filling').click()
-      cy.findByText('Closed Without Filling')
+        cy.findByText('< Back to Orders').click()
+        cy.findByText('Show Closed Orders').parent().click()
 
-      cy.findByText('< Back to Orders').click()
+        // confirm order is closed with order count
+        cy.findByRole('table').within(() => {
+          cy.findByText('Opened At').click()
 
-      cy.findByText('Show Closed Orders').parent().click()
-      // confirm order is closed with order count
-      cy.findByRole('table').within(() => {
-        cy.findByText('Opened At').click()
+          const row = cy.findByText(job.employmentTitle).parent().parent().parent()
 
-        const row = cy.findByText(job.employmentTitle).parent().parent().parent()
-
-        row.within(() => {
-          cy.findByText('Closed Without Filling')
-          cy.findByText('12')
+          row.within(() => {
+            cy.findByText('Closed Without Filling')
+            cy.findByText('12')
+          })
         })
 
-        cy.findByText(job.employmentTitle).click()
-      })
+        cy.findByText('Show Closed Orders').parent().click()
 
-      // reactivate order
-      cy.findByText('Reactivate Job Order').click()
-      cy.findByText('Open')
+        cy.findByText('Create New Job Order').click()
+        cy.findByText('Create A New Job Order')
+
+        cy.findByLabelText('Select Job').select(`${job.employmentTitle}: ${employer.name}`)
+        cy.findByText("Save").click()
+
+        cy.findByRole('table').within(() => {
+          cy.findByText('Opened At').click()
+
+          const row = cy.findByText(job.employmentTitle).parent().parent().parent()
+
+          row.within(() => {
+            cy.findByText('Provide Order Count')
+            cy.findByText('Needs Order Count')
+          })
+
+          cy.findByText(job.employmentTitle).click()
+        })
+      })
     })
   })
 })
