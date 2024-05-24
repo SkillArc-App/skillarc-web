2d09ab66a3844aa66a43df74e6a65a25e6646dda
John Chabot <johnchabot2013@gmail.com>
2024-05-24 14:43:16 -0400

    Point query to get instead of create

--- src/app/onboarding/hooks/useOnboardingQuery.ts
+++ src/app/onboarding/hooks/useOnboardingQuery.ts
@@ -2,7 +2,7 @@
 
 import { OnboardingNextStep } from '@/common/types/OnboardingData'
 import { useAuthenticatedQuery } from '../../../frontend/hooks/useAuthenticatedQuery'
-import { post } from '../../../frontend/http-common'
+import { get } from '../../../frontend/http-common'
 
 export type OnboardingResponse = {
   nextStep: OnboardingNextStep
@@ -14,5 +14,5 @@ export const useOnboardingQuery = () =>
   useAuthenticatedQuery<OnboardingResponse>(
     ['onboarding_data'],
     async ({ token }) =>
-      (await post(`${process.env.NEXT_PUBLIC_API_URL}/onboarding_sessions`, {}, token)).data,
+      (await get(`${process.env.NEXT_PUBLIC_API_URL}/onboarding_sessions`, token)).data,
   )
