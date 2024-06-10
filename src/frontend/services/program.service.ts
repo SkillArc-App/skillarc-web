b5ef9beac212f5235f850c9cf8e743a6619d81e0
John Chabot <johnchabot2013@gmail.com>
2024-06-10 17:10:36 -0400

    Slim down "services"

--- src/frontend/services/program.service.ts
+++ src/frontend/services/program.service.ts
@@ -8,24 +8,11 @@ type Program = {
   trainingProviderId: string
 }
 
-const getAll = async (token: string) => {
-  // const res = await http.get<Program[]>(`/api/programs`)
-  const res = await get<Program[]>(`/programs`, token)
-  return res.data
-}
-
-const getAllForTrainingProvder = async (token: string) => {
-  const res = await get<Program[]>(`/training_providers/programs`, token)
-  return res.data
-}
-
 const getOne = async (id: string) => {
   const res = await get<Program>(`/api/programs/${id}`)
   return res.data
 }
 
 export const FrontendProgramService = {
-  getAll,
-  getAllForTrainingProvder,
   getOne,
 }
