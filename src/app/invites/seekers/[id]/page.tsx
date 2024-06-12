6ecdc4a8b4e5d087cd7ffe555727934ce675d178
John Chabot <johnchabot2013@gmail.com>
2024-06-12 13:16:25 -0400

    Update seeker invite to just be a redirect

--- src/app/invites/seekers/[id]/page.tsx
+++ src/app/invites/seekers/[id]/page.tsx
@@ -1,20 +1,7 @@
 'use client'
 
 import { LoadingPage } from '@/frontend/components/Loading'
-import { useFixedParams } from '@/frontend/hooks/useFixParams'
-import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
 
 export default function SeekerInvite() {
-  const router = useRouter()
-  const seekerInviteId = useFixedParams('id')?.['id']
-
-  useEffect(() => {
-    if (seekerInviteId) {
-      localStorage.setItem('seekerInviteCode', seekerInviteId)
-      router.push('/')
-    }
-  }, [router, seekerInviteId])
-
   return <LoadingPage />
 }
