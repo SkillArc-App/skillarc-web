385dbbce39ad4520698fb8c3bcfbca2b265f38e8
John Chabot <johnchabot2013@gmail.com>
2024-07-17 13:19:45 -0400

    Add screener hooks for questions

---
+++ src/app/screeners/hooks/useAddQuestionsMutation.ts
@@ -0,0 +1,22 @@
+import { Questions } from '@/app/screeners/types'
+import { useAuthenticatedMutation } from '@/frontend/hooks/useAuthenticatedMutation'
+import { post } from '@/frontend/http-common'
+import { useQueryClient } from '@tanstack/react-query'
+
+export const useAddQuestionsMutation = ({ onSuccess }: { onSuccess: () => void }) => {
+  const queryClient = useQueryClient()
+
+  return useAuthenticatedMutation(
+    async (questions: Questions, token: string) => {
+      const result = await post(`screeners/questions/`, questions, token)
+
+      return result.data
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['questions'])
+        onSuccess()
+      },
+    },
+  )
+}
---
+++ src/app/screeners/hooks/useUpdateQuestionsMutation.ts
@@ -0,0 +1,22 @@
+import { Questions } from '@/app/screeners/types'
+import { useAuthenticatedMutation } from '@/frontend/hooks/useAuthenticatedMutation'
+import { put } from '@/frontend/http-common'
+import { useQueryClient } from '@tanstack/react-query'
+
+export const useUpdateQuestionsMutation = ({ onSuccess }: { onSuccess: () => void }) => {
+  const queryClient = useQueryClient()
+
+  return useAuthenticatedMutation(
+    async (questions: Questions, token: string) => {
+      const result = await put(`screeners/questions/${questions.id}`, questions, token)
+
+      return result.data
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['questions'])
+        onSuccess()
+      },
+    },
+  )
+}
