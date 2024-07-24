9fa266c807a8cca543a3a05b957a4f6a05e4f40e
John Chabot <johnchabot2013@gmail.com>
2024-07-24 13:52:35 -0400

    Do idiomatic favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,9 +10,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
