c6acf823915b447d5253e3ab2b0dcf51dfb917b8
John Chabot <johnchabot2013@gmail.com>
2024-07-29 12:47:32 -0400

    Allow mock auth in "production"

--- app/admin/hooks/useAllUsers.ts
+++ app/admin/hooks/useAllUsers.ts
@@ -3,9 +3,6 @@ import { get } from '../../http-common'
 
 export const useAllUsers = () => {
   const allUsersQuery = useQuery(['allUsers'], () => {
-    if (process.env.NODE_ENV !== 'development') {
-      return Promise.reject('No user id')
-    }
     const getAll = async () => {
       const res = await get<{ id: string; email: string; sub: string }[]>('/admin/users')
 
