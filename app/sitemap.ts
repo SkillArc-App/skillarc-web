828327b8a2525bf3ae447bff273f731b42b92770
John Chabot <johnchabot2013@gmail.com>
2024-08-01 15:50:04 -0400

    Update sitemap with jobs

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,26 +1,45 @@
 import { MetadataRoute } from 'next'
+import { SearchJob } from './common/types/Search'
+import { get } from './http-common'
 
 const getBaseUrl = () => {
   if (process.env.NEXT_PUBLIC_ENVIRONMENT_URL) {
     return process.env.NEXT_PUBLIC_ENVIRONMENT_URL
   }
 
-  throw "Base URL not set"
+  throw 'Base URL not set'
 }
 
-export default function sitemap(): MetadataRoute.Sitemap {
+const getJobs = async (): Promise<SearchJob[]> => {
+  if (process.env.NODE_ENV != 'production') {
+    return []
+  }
+
+  return (await get<SearchJob[]>('seekers/jobs')).data
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const jobs = await getJobs()
+  const jobSites: MetadataRoute.Sitemap = jobs.map((job) => ({
+    url: `${getBaseUrl()}/jobs/${job.id}`,
+    lastModified: new Date(),
+    priority: 0.8,
+    changeFrequency: 'weekly',
+  }))
+
   return [
     {
       url: getBaseUrl(),
       lastModified: new Date(),
       priority: 0.5,
-      changeFrequency: 'monthly'
+      changeFrequency: 'monthly',
     },
     {
       url: `${getBaseUrl()}/jobs`,
       lastModified: new Date(),
       priority: 1.0,
-      changeFrequency: 'daily'
+      changeFrequency: 'daily',
     },
+    ...jobSites
   ]
 }
