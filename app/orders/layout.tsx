ddb2d03e7bde7ded9a0ee37cd7ce2d0860df1c11
John Chabot <johnchabot2013@gmail.com>
2024-08-07 12:20:07 -0400

    Add page metadata to coaches and order dash

--- app/orders/layout.tsx
+++ app/orders/layout.tsx
@@ -1,49 +1,19 @@
-'use client'
-
 import { LoadingPage } from '@/components/Loading'
-import { useUser } from '@/hooks/useUser'
-import { Box, GridItem, SimpleGrid, Tab, TabList, TabPanels, Tabs } from '@chakra-ui/react'
-import Link from 'next/link'
-import { redirect, usePathname } from 'next/navigation'
+import { Metadata } from 'next'
+import { Suspense } from 'react'
+import LayoutClient from './layout-client'
 
-const JobOrderLayout = ({ children }: { children: React.ReactNode }) => {
-  const { data: user, isLoading } = useUser()
-  const pathName = usePathname()
+export const metadata: Metadata = {
+  title: 'SkillArc: Job Orders Dashboard',
+  description: 'The SkillArc Orders Dashboard',
+}
 
-  if (isLoading) return <LoadingPage />
-  if (!user) return <LoadingPage />
-  if (!user.userRoles?.some((ur) => ur.role.name === 'job_order_admin')) {
-    redirect('/')
-  }
-
-  const tabs: Record<string, number> = {
-    '/orders/orders': 0,
-    '/orders/questions': 1,
-  }
-
-  const index = tabs[pathName] || 0
-
+const Orders = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SimpleGrid pt={'1rem'} columns={12} height={'100%'} overflow={'clip'} width={'100%'}>
-      <GridItem colSpan={1} />
-      <GridItem px={'1rem'} pt={'1rem'} overflow={'scroll'} colSpan={10}>
-        <Tabs height={'100%'} variant={'enclosed'} index={index}>
-          <TabList>
-            <Tab as={Link} href={'/orders/orders'}>
-              Orders
-            </Tab>
-            <Tab as={Link} href={'/orders/questions'}>
-              Screener Questions
-            </Tab>
-          </TabList>
-          <TabPanels>
-            <Box pt={'2rem'}>{children}</Box>
-          </TabPanels>
-        </Tabs>
-      </GridItem>
-      <GridItem colSpan={1} />
-    </SimpleGrid>
+    <Suspense fallback={<LoadingPage />}>
+      <LayoutClient>{children}</LayoutClient>
+    </Suspense>
   )
 }
 
-export default JobOrderLayout
+export default Orders
