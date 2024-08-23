12af822c9476d1657090fb5ac906674aa717366f
John Chabot <johnchabot2013@gmail.com>
2024-08-23 16:19:04 -0400

    Update notes page with timeline

--- app/coaches/contexts/[id]/notes/page.tsx
+++ app/coaches/contexts/[id]/notes/page.tsx
@@ -1,14 +1,42 @@
 'use client'
 
 import { useCoachSeekerData } from '@/coaches/hooks/useCoachSeekerData'
+import { useContactMutation } from '@/coaches/hooks/useContactMutation'
+import { ContactDirection, ContactType } from '@/coaches/types'
 import { IdParams } from '@/common/types/PageParams'
 import { LoadingPage } from '@/components/Loading'
-import NotesList from '@/components/NoteList'
+import TimeLine, { TimeLineItem } from '@/components/TimeLine'
 import { useAuthToken } from '@/hooks/useAuthToken'
 import { destroy, post, put } from '@/http-common'
+import { HStack, IconButton, Radio, RadioGroup, Stack, Tooltip } from '@chakra-ui/react'
+import { useState } from 'react'
+import { FaComment, FaEnvelope, FaPhone } from 'react-icons/fa6'
+
+function contactIcon(contactType: ContactType) {
+  return contactType === ContactType.EMAIL ? (
+    <FaEnvelope />
+  ) : contactType === ContactType.SMS ? (
+    <FaComment />
+  ) : (
+    <FaPhone />
+  )
+}
+
+function contactTypeNote(contactType: ContactType) {
+  switch (contactType) {
+    case ContactType.EMAIL:
+      return 'Email'
+    case ContactType.PHONE:
+      return 'Phone Call'
+    case ContactType.SMS:
+      return 'Text'
+  }
+}
 
 const Notes = ({ params: { id } }: IdParams) => {
   const { data: seeker, refetch: refetchSeeker } = useCoachSeekerData(id)
+  const contact = useContactMutation()
+  const [direction, setDirection] = useState<ContactDirection>(ContactDirection.SENT)
 
   const token = useAuthToken()
 
@@ -54,13 +82,77 @@ const Notes = ({ params: { id } }: IdParams) => {
 
   if (!seeker) return <LoadingPage />
 
+  const notes: TimeLineItem[] = seeker.notes.map((note) => ({ kind: 'note', ...note }))
+
+  const markers: TimeLineItem[] = seeker.contacts.map(
+    ({ note, contactedAt, contactType, contactDirection }) => {
+      return {
+        kind: 'marker',
+        date: contactedAt,
+        note: `${contactTypeNote(contactType)} ${contactDirection} ${note}`,
+        icon: contactIcon(contactType),
+      }
+    },
+  )
+
+  const items = notes.concat(markers)
+
   return (
-    <NotesList
-      notes={seeker.notes}
-      onCreate={addNote}
-      onDelete={deleteNote}
-      onModify={modifyNote}
-    />
+    <Stack>
+      <HStack>
+        <RadioGroup onChange={(value) => setDirection(value as ContactDirection)} value={direction}>
+          <Stack direction="row">
+            <Radio value={ContactDirection.SENT}>Sent</Radio>
+            <Radio value={ContactDirection.RECEIVED}>Received</Radio>
+          </Stack>
+        </RadioGroup>
+        <Tooltip label={`Seeker Email Communication Occurred`}>
+          <IconButton
+            aria-label="Email Communication"
+            onClick={() =>
+              contact.mutate({
+                id,
+                contactDirection: direction,
+                contactType: ContactType.EMAIL,
+                note: '',
+              })
+            }
+            icon={contactIcon(ContactType.EMAIL)}
+          />
+        </Tooltip>
+
+        <Tooltip label={`Seeker Text Communication Occurred`}>
+          <IconButton
+            aria-label="Text Communication"
+            onClick={() =>
+              contact.mutate({
+                id,
+                contactDirection: direction,
+                contactType: ContactType.SMS,
+                note: '',
+              })
+            }
+            icon={contactIcon(ContactType.SMS)}
+          />
+        </Tooltip>
+
+        <Tooltip label={`Seeker Phone Communication Occurred`}>
+          <IconButton
+            aria-label="Phone Communication"
+            onClick={() =>
+              contact.mutate({
+                id,
+                contactDirection: direction,
+                contactType: ContactType.PHONE,
+                note: '',
+              })
+            }
+            icon={contactIcon(ContactType.PHONE)}
+          />
+        </Tooltip>
+      </HStack>
+      <TimeLine items={items} onCreate={addNote} onDelete={deleteNote} onModify={modifyNote} />
+    </Stack>
   )
 }
 
