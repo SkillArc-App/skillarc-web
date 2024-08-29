5398ed807b5eec79e25c2936b8bce7288a14d03e
John Chabot <johnchabot2013@gmail.com>
2024-08-29 10:01:20 -0400

    Add gtm search event

--- app/hooks/useDebounce.ts
+++ app/hooks/useDebounce.ts
@@ -1,17 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
-export function useDebounce<T>(value: T, delay: number) {
-  const [debouncedValue, setDebouncedValue] = useState(value);
+export function useDebounce<T>(value: T, delay: number, callback?: (value: T) => void) {
+  const [debouncedValue, setDebouncedValue] = useState(value)
 
   useEffect(() => {
     const handler = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
+      setDebouncedValue(value)
+
+      if (callback) {
+        callback(value)
+      }
+    }, delay)
 
     return () => {
-      clearTimeout(handler);
-    };
-  }, [value, delay]);
+      clearTimeout(handler)
+    }
+  }, [value, delay, callback])
 
-  return debouncedValue;
+  return debouncedValue
 }
